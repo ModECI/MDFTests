e59595711f3bce9a59ecf9dc9fd45b7e0313d8a2
Padraig  Gleeson <p.gleeson@gmail.com>
2020-11-18 17:39:57 +0000

    Add image of net

--- NeuroML/regenerateAndTest.sh
+++ NeuroML/regenerateAndTest.sh
@@ -10,8 +10,11 @@ python ABCD.py -pnl
 ####  Load in PsyNeuLink version & run
 python test_bids_import.py 
 
-####  Generate a graph depicting the structure of the network from the LEMS description
+####  Generate a graph depicting the structure & *dynamics* of the network from the LEMS description
 jnml LEMS_SimABCD.xml -graph
+
+####  Generate a graph depicting the structure of network from NeuroMLlite
+python ABCD.py -graph2
 
 
 ####  Generate and run jNeuroML version of the network from NeuroMLlite definition
@@ -22,3 +25,5 @@ python FN.py -pnl
 
 ####  Generate a graph depicting the structure of the network from the LEMS description
 jnml LEMS_SimFN.xml -graph
+
+echo "Successfully ran all tests"
