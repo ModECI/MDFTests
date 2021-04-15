5113f99cd174053dde1d0c18ba56cd5e0af7d825
Brian Broll <brian.broll@gmail.com>
2021-04-15 14:37:21 -0500

    Add MDF seed

--- mdf_gme/config/config.webgme.js
+++ mdf_gme/config/config.webgme.js
@@ -9,6 +9,7 @@ var config = require('webgme/config/config.default'),
 // The paths can be loaded from the webgme-setup.json
 config.plugin.basePaths.push(__dirname + '/../src/plugins');
 config.plugin.basePaths.push(__dirname + '/../node_modules/webgme-json-importer/src/plugins');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/MDF');
 
 
 
